82c91055d4abd168a65ef025c2c378ec7576e4ac
VitorSquina <89233635+VitorSquina@users.noreply.github.com>
2022-02-03 18:44:29 -0300

    arquivos e codigos da 1 beta do SquareGame

---
+++ index.js
@@ -0,0 +1,12 @@
+let n = 6;
+let i = 1;
+let contador = 0;
+
+function contadorfuncao(){
+    while (contador < n){
+        i++;
+        contador++;
+        console.log(contador)
+    }
+    contador = 0;
+}
